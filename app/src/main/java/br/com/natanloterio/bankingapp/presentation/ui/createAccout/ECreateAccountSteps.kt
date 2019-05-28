6a70d501374a1c8963e72b21b60761d825185acb
natanloterio <natanloterio@gmail.com>
2019-05-28 20:53:16 -0300

    Adding styling to the app

--- app/src/main/java/br/com/natanloterio/bankingapp/presentation/ui/createAccout/ECreateAccountSteps.kt
+++ app/src/main/java/br/com/natanloterio/bankingapp/presentation/ui/createAccout/ECreateAccountSteps.kt
@@ -1,9 +1,9 @@
 package br.com.natanloterio.bankingapp.presentation.ui.createAccout
     enum class ECreateAccountSteps{
-        JUST_A_FRIENDLY_NOTE,
-        ORIENTATIONS_BEFORE_WE_GO,
         PRESENTATION,
         WHAT_YOU_WILL_HAVE,
+        ORIENTATIONS_BEFORE_WE_GO,
+        JUST_A_FRIENDLY_NOTE,
         YOUR_BASIC_INFO,
     }
 
